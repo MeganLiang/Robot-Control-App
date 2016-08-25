6e7e213dd9b2b8ba66f4c88bfaad5ac675449538
Michael Pearson <zl2mjp@gmail.com>
2016-08-25 17:30:52 +1200

    Use the correct port

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 var http = require('http');
+var port = process.env.PORT || 3000;
+
 
 var server = http.createServer(function (request, response) {
     response.end("Hello world!");
-}).listen(3000);
+}).listen(port);
